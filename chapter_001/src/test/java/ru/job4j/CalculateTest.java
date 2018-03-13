35c4e89d1b3da5d708921599676a49ec98b6a893
Rachehan <igorkkhm@gmail.com>
2018-03-13 20:38:21 +0300

    1.4 JUnit[#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**h
+ * Test.
+ * 
+ * @author Igor Khmelevskiy (igorkkhm@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+	/**
+	 * Test echo.
+	 */ @Test
+	 public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Igor Khmelevskiy";
+		String expect = "Echo, echo, echo : Igor Khmelevskiy";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+
+}
