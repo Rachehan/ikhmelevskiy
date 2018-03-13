efd555a0aa1f2cf8d9fe559f1d30c5a16576e53c
Rachehan <igorkkhm@gmail.com>
2018-03-13 19:41:30 +0300

    1.3. JavaDoc Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
 
+/**
+ * Print "Hello world"
+ *
+ * @author Igor Khmelevskiy (igorkkhm@gmail.com)
+ */
 public class Calculate {
 
+	/**
+	 * Main.
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Hello world");
